ae0cc8a7101545198a3c5bbd8094eb2f0f988126
Unknown <unknown@example.com>
2020-02-04 16:00:21 +0330

    :heavy_plus_sign: city migration

---
+++ database/migrations/2020_02_04_121624_create_city_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCityTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cities', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('slug');
+            $table->integer('state_id')->unsigned();
+            $table->timestamps();
+            $table->foreign('state_id')
+                ->references('id')
+                ->on('states')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cities');
+    }
+}
