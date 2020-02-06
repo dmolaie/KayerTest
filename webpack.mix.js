b6f9bf22c024c9ef7f6b821c65cfff5945919ca6
Mohammad Soleimanian <mohamad.s@fanap.plus>
2020-02-06 19:59:04 +0330

    front/refactor webpack.mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,19 +1,12 @@
 const mix = require('laravel-mix');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
+mix.webpackConfig({
+    resolve: {
+        alias: {
+            '@vendor': path.resolve(__dirname, 'resources/vendor/'),
+        }
+    }
+});
 
 mix
-    .js('resources/js/master.js', 'public/js')
-    .js('resources/js/home.js', 'public/js')
-    .js('resources/js/his-page.js', 'public/js')
-    .js('resources/js/org-page.js', 'public/js')
-    .sass('resources/sass/style.sass', 'public/css');
+    .js('resources/js/Site/master.js', 'public/js');
