0b9a061b4f30dd2a355af5e51ed82dc1cca578b1
Mohammad Soleimanian <mohamad.s@fanap.plus>
2020-02-09 18:08:25 +0330

    front/news page

---
+++ resources/js/Site/news_show.js
@@ -0,0 +1,44 @@
+import Swiper from 'swiper';
+
+const SpaceBetweenItem = 32;
+
+try {
+    const MountRelativeNewsCarousel = () => {
+        try {
+            let eventsCarousel = new Swiper('.n-show-page .relative-news .carousel__container', {
+                slidesPerView: 'auto',
+                spaceBetween: SpaceBetweenItem,
+                watchOverflow: true,
+                resistanceRatio: 0,
+                breakpoints: {
+                    0: {
+                        slidesPerView: 1,
+                        spaceBetween: 11,
+                    },
+                    520: {
+                        slidesPerView: 2,
+                        spaceBetween: SpaceBetweenItem,
+                    },
+                    1000: {
+                        slidesPerView: 3,
+                        spaceBetween: SpaceBetweenItem,
+                    },
+                    1300: {
+                        slidesPerView: 4,
+                        spaceBetween: SpaceBetweenItem,
+                    }
+                }
+            });
+        } catch ( e ) {
+            //
+        }
+    };
+
+    window.addEventListener(
+        'load', () => {
+            MountRelativeNewsCarousel()
+        }
+    )
+} catch (e) {
+    //
+}
