88303af0741370b6f2ba7bc6e993ffa637d89b8e
Mohammad Soleimanian <mohamad.s@fanap.plus>
2020-02-09 15:14:43 +0330

    front/added general plugin

--- resources/BackOffice/app.js
+++ resources/BackOffice/app.js
@@ -2,10 +2,12 @@ import Vue from 'vue';
 import VueRouter from "vue-router";
 import routes from "./services/routes";
 import App from '@components/App.vue';
+import General from '@vendor/plugin/general';
 import Notification from '@vendor/components/notification';
 
 Vue.config.devtools = true;
 
+Vue.use( General );
 Vue.use( VueRouter );
 Vue.use( Notification );
 
