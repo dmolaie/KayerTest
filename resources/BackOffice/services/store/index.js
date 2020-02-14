246d2f8e14a820852c71b2ba9bd60c114c75e788
Mohammad Soleimanian <mohamad.s@fanap.plus>
2020-02-14 22:17:40 +0330

    front/added vuex

---
+++ resources/BackOffice/services/store/index.js
@@ -0,0 +1,29 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+const state = {
+
+};
+
+const getters = {
+
+};
+
+const mutations = {
+
+};
+
+const modules = {
+
+};
+
+Vue.use( Vuex );
+
+const Store = new Vuex.Store({
+    state,
+    getters,
+    mutations,
+    modules
+});
+
+export default Store;
