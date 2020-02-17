b5d080598d97803a23f57d453d756e3536f96182
Mohammad Soleimanian <mohamad.s@fanap.plus>
2020-02-17 19:27:38 +0330

    front/added mission-and-vision blade

---
+++ domains/Site/resources/views/fa/pages/mission-and-vision.blade.php
@@ -0,0 +1,68 @@
+@extends('fa.template.master')
+    @section('content')
+        <div class="miv-page i-page">
+            <div class="container">
+                <h1 class="flex text-blue font-24 md:font-22 text-center">
+                    <span class="flower_line flower_line--blue--200 relative flex items-end justify-end flex-1 reverse-item m-0"></span>
+                    <span class="i-page__title">
+                        ماموریت و چشم‌انداز
+                    </span>
+                    <span class="flower_line flower_line--blue--200 relative flex items-end justify-end flex-1 m-0"></span>
+                </h1>
+                <div class="inner-box inner-box--white">
+                    <p class="text-blue-800 font-lg font-bold cursor-default m-b-26">
+                        ماموریت :
+                    </p>
+                    <p class="i-page__text md:font-sm text-bayoux font-base text-right m-b-70">
+                        رسالت انجمن اهدای عضو ایرانیان که بصورت یک سازمان مردم نهاد غیر انتفاعی، غیر سیاسی و غیر دولتی توسط عده ای از متخصصین فراهم آوری و پیوند اعضا و فرهیختگان عرصه هنر و فرهنگ کشور در سال ۱۳۹۴ تاسیس گردیده تلاش در جهت استفاده بهینه از اعضای همه افراد مرگ مغزی مناسب اهدا در جهت نجات بیماران نیازمند پیوند می باشد. در این راستا انجمن با ارتقای یکپارچه آمار اهدای عضو در کشور در نهادینه سازی این فرهنگ نوپا در ایران می کوشد. همچنین با آموزش تخصصی هماهنگ  کنندگان اهدای عضو دانش این افراد را برای شناسایی ، مراقبت، رویارویی با خانواده اهداکننده و سایر مراحل فراهم آوری اعضا بالا برده و در بدین وسیله موجب افزایش کمیت و کیفیت اعضای اهدا شده می گردد. با حمایت از پژوهش در زمینه اهدای عضو انجمن تلاش دارد که اطلاعات همه تیم های اهدای عضو را به روز نگه داشته و کاری کند که ایران یکی از منابع علمی معتبر دنیا باشد. همچنین با حمایت از خانواده های اهداکننده عضو و توانبخشی آنان تلاش می کند که قدردانی جامعه از اقدام مسئولانه و ایثارگرانه این خانواده ها را به آنها نشان دهد. در نهایت انجمن در کنار وزارت بهداشت آمادگی پذیرش مسئولیت و راهبری همه مواردی را که در ارتقای اهدای عضو و پیوند اعضا در کشور ضروری بوده ولی در بدنه دولت به سختی انجام پذیر است را دارد.
+                    </p>
+                    <p class="relative ext-blue-800 font-lg font-bold cursor-default m-b-26">
+                        <span class="miv-page__top-flower i-page__flower_line i-page__flower_line--left flower_line flower_line--blue--200 absolute flex items-end justify-end pointer-event-none m-0"></span>
+                        چشم انداز :
+                    </p>
+                    <p class="relative i-page__text md:font-sm text-bayoux font-base text-right m-b-70">
+                        انجمن اهدای عضو ایرانیان در پی روزی است که هیچ فرد مرگ مغزی اعضای ارزشمند خود را با خود به زیر خاک نبرد و هیچ انسان نیازمند پیوندی در لیست انتظار به دلیل نرسیدن عضو پیوندی جان خود را از دست ندهد
+                        <span class="miv-page__bottom-flower i-page__flower_line i-page__flower_line--right flower_line flower_line--blue--200 absolute flex items-end justify-end reverse-item pointer-event-none m-0"></span>
+                    </p>
+                    <div class="i-page__footer relative">
+                        <div class="flex items-center md:flex-col">
+                            <p class="text-bayoux font-sm-bold cursor-default md:m-b-20">
+                                انتشار: ۱۱:۲۹ / ۱۸ فروردین ۱۳۹۷
+                            </p>
+                            <div class="inline-flex items-center l:m-r-auto md:m-b-20">
+                                <span class="text-bayoux font-sm-bold cursor-default">
+                                    لینک کوتاه:
+                                </span>
+                                <div class="clipboard relative cursor-pointer text-blue-800 font-sm border border-solid i-page__box rounded-1/2 m-r-10">
+                                    <span class="clipboard__message absolute w-full h-full flex items-center font-xs-bold opacity-0 transition-opacity pointer-event-none user-select-none">
+                                        کپی شد
+                                    </span>
+                                    https://ehda.center/PNWVzX
+                                </div>
+                            </div>
+                            <div class="flex items-center border border-solid i-page__box rounded-1/2 l:m-r-20 user-select-none">
+                                <a href=""
+                                   target="_blank"
+                                   class="i-page__social ic--gmail transition-opacity opacity-80 l:hover:opacity-1"></a>
+                                <a href=""
+                                   target="_blank"
+                                   class="i-page__social ic--facebook transition-opacity opacity-80 l:hover:opacity-1"></a>
+                                <a href=""
+                                   target="_blank"
+                                   class="i-page__social ic--telegram transition-opacity opacity-80 l:hover:opacity-1"></a>
+                                <a href=""
+                                   target="_blank"
+                                   class="i-page__social ic--twitter transition-opacity opacity-80 l:hover:opacity-1"></a>
+                                <a href=""
+                                   target="_blank"
+                                   class="i-page__social ic--pinterest transition-opacity opacity-80 l:hover:opacity-1"></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endsection
+
+    @section('scripts')
+    @endsection
