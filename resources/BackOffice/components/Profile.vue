7ad1424a4624307976b42b93b33315206f753f47
Mohammad Soleimanian <mohamad.s@fanap.plus>
2020-02-18 10:12:39 +0330

    front/profile route

---
+++ resources/BackOffice/components/Profile.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="">
+        <h3>
+            Profile
+        </h3>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Profile"
+    }
+</script>
