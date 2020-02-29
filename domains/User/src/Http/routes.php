86551fa83298a8cab027428c82f47a8398e53cb8
DARIUSH <dariush@fanap.plus>
2020-02-29 16:31:51 +0330

    bug fix

--- domains/User/src/Http/routes.php
+++ domains/User/src/Http/routes.php
@@ -4,6 +4,8 @@ Route::group(['middleware' => ['web']], function () {
 
     Route::get('/full-info', 'UserController@getFullUserInfo')->middleware('auth');
     Route::post('/update-info', 'UserController@updateUserInfo')->middleware('auth');
+    Route::post('/validate_data_user_client', 'UserController@ValidateDataUserClient');
+    Route::post('/validate_data_user_legate', 'UserController@ValidateDataUserLegate');
 });
 Route::get('/user-search', 'UserController@getListForAdmin')->middleware('auth:api')->name('user-search');
 Route::post('/register', 'UserController@register')->name('register-ehda');
