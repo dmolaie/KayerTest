94e1a65570e9da272b205718e160faa63e73c0b1
Maryam <maryam.b@fanap.plus>
2020-03-07 14:26:48 +0330

    add logout api

--- domains/Admin/resources/lang/fa/response.php
+++ domains/Admin/resources/lang/fa/response.php
@@ -4,6 +4,7 @@
 return [
     'authenticate' => [
         'successful_login' => 'ورود با موفقیت انجام شد.',
+        'successful_logout' => 'خروج با موفقیت انجام شد.',
         'error_username_password' => 'نام کاربری و پسورد اشتباه است'
     ],
 ];
