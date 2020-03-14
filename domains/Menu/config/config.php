79789b28fa4eb2393acb885790a6608ced81669d
Maryam <maryam.b@fanap.plus>
2020-03-14 18:58:32 +0330

    fix menu list priority

--- domains/Menu/config/config.php
+++ domains/Menu/config/config.php
@@ -2,12 +2,12 @@
 
 return [
     'menus_type'       => [
-        'article_type' =>'article',
-        'link_type' =>'link',
-        'separator_type' =>'separator',
-        'list_news_type' =>'list_news',
-        'list_event_type' =>'list_event',
-        'list_article_type' =>'list_article'
+        'article_type'      => 'article',
+        'link_type'         => 'link',
+        'separator_type'    => 'separator',
+        'list_news_type'    => 'list_news',
+        'list_event_type'   => 'list_event',
+        'list_article_type' => 'list_article'
     ],
     'menu_language'    => [
         'fa',
