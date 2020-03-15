a016fdd90030d26f516827d4ccbaccf94ca5d465
Mohammad Soleimanian <mohamad.s@fanap.plus>
2020-03-15 09:03:41 +0330

    front/remove logout box - Dashboard.vue

--- resources/BackOffice/views/Dashboard.vue
+++ resources/BackOffice/views/Dashboard.vue
@@ -1,13 +1,5 @@
 <template>
     <div class="dashboard">
-        <nav class="dashboard-p__nav bg-white w-full flex items-center">
-            <router-link
-                :to="{ name: 'LOGOUT' }"
-                class="dashboard-p__nav_user flex items-center text-blue"
-                >
-                logout
-            </router-link>
-        </nav>
         <div class="dashboard-p__content w-full text-center font-24 font-bold">
             به پنل مدیریتی انجمن اهدا خوش آمدید
         </div>
