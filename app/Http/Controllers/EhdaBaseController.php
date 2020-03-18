cd1ae3f06f9db0b5e0a60ac70928c88c2b6094b3
DARIUSH <dariush@fanap.plus>
2020-03-18 16:27:45 +0330

    remove header

--- app/Http/Controllers/EhdaBaseController.php
+++ app/Http/Controllers/EhdaBaseController.php
@@ -12,6 +12,6 @@ class EhdaBaseController extends Controller
             'data' => $data,
             'status_code' => $code,
             'message' => $message
-        ], $code)->header('Access-Control-Allow-Headers' , 'Content-Type, X-CSRF-TOKEN, Access-Control-Allow-Origin, Accept, Authorization');
+        ], $code);
     }
 }
