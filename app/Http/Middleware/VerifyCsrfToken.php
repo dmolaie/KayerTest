e69eff3d20fcb0e3734b32a61f4be8173db6f4a7
DARIUSH <dariush@fanap.plus>
2020-03-24 20:16:18 +0430

    change verifyCsrfToken

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -23,4 +23,14 @@ class VerifyCsrfToken extends Middleware
         'user/logout',
         'http://ehdacenter.com/*'
     ];
+
+    protected function getTokenFromRequest($request)
+    {
+        $token = $request->input('_token') ?: $request->header('X-CSRF-TOKEN');
+
+        if (! $token && $header = $request->header('X-XSRF-TOKEN')) {
+            $token = $this->encrypter->decrypt($header, static::serialized());
+        }
+        return $token;
+    }
 }
