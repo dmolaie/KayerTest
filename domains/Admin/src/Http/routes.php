8e4d787bea262dbb60e1e2cfab7763f6bc63d17e
DARIUSH <dariush@fanap.plus>
2020-03-24 17:34:46 +0430

    clear test csrf token

--- domains/Admin/src/Http/routes.php
+++ domains/Admin/src/Http/routes.php
@@ -2,7 +2,7 @@
 
 Route::prefix('user')->name('admin.')->group(function() {
     Route::get('/', 'HomeController@show')->name('login')->middleware('web');
-    Route::post('/login', 'LoginController@login')/*->middleware('web')*/;
+    Route::post('/login', 'LoginController@login')->middleware('web');
     Route::post('/logout', 'LoginController@logout')->name('logout')->middleware('web');
 
 });
