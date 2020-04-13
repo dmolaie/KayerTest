67ca3402e923ebbd4d8452fa2eba33a3d23dbf2e
Maryam <maryam.b@fanap.plus>
2020-04-13 10:40:44 +0430

    fix news

--- domains/News/src/Http/Presenters/NewsInfoPresenter.php
+++ domains/News/src/Http/Presenters/NewsInfoPresenter.php
@@ -4,6 +4,7 @@
 namespace Domains\News\Http\Presenters;
 
 
+use Carbon\Carbon;
 use Domains\News\Services\Contracts\DTOs\NewsInfoDTO;
 
 class NewsInfoPresenter
@@ -18,41 +19,69 @@ class NewsInfoPresenter
     public function transform(NewsInfoDTO $newsInfoDTO)
     {
         return [
-            'id' => $newsInfoDTO->getId(),
-            'first_title' => $newsInfoDTO->getFirstTitle(),
-            'second_title' => $newsInfoDTO->getSecondTitle(),
-            'abstract' => $newsInfoDTO->getAbstract(),
-            'description' => $newsInfoDTO->getDescription(),
-            'category' => $newsInfoDTO->getCategory() ? $newsInfoDTO->getCategory() : null,
-            'publish_date' => strtotime($newsInfoDTO->getPublishDate()),
-            'source_link' => $newsInfoDTO->getSourceLink(),
-            'status' => [
-                'fa' => $this->getNewsStatus($newsInfoDTO),
-                'en' => $newsInfoDTO->getStatus()
-            ],
-            'province' => [
-                'id' => $newsInfoDTO->getProvince()->id,
+            'id'                 => $newsInfoDTO->getId(),
+            'first_title'        => $newsInfoDTO->getFirstTitle(),
+            'second_title'       => $newsInfoDTO->getSecondTitle(),
+            'abstract'           => $newsInfoDTO->getAbstract(),
+            'description'        => $newsInfoDTO->getDescription(),
+            'category'           => $newsInfoDTO->getCategory() ? $newsInfoDTO->getCategory() : null,
+            'publish_date'       => strtotime($newsInfoDTO->getPublishDate()),
+            'source_link'        => $newsInfoDTO->getSourceLink(),
+            'status'             => $this->getStatus($newsInfoDTO),
+            'is_created_by_user' => $this->isCreatedByUser($newsInfoDTO),
+            'slug'               => $newsInfoDTO->getSlug(),
+            'province'           => [
+                'id'   => $newsInfoDTO->getProvince()->id,
                 'name' => $newsInfoDTO->getProvince()->name,
             ],
-            'publisher' => [
-                'id' => $newsInfoDTO->getPublisher()->id,
-                'name' => $newsInfoDTO->getPublisher()->name,
+            'publisher'          => [
+                'id'        => $newsInfoDTO->getPublisher()->id,
+                'name'      => $newsInfoDTO->getPublisher()->name,
                 'last_name' => $newsInfoDTO->getPublisher()->last_name
             ],
-            'editor' => $newsInfoDTO->getEditor() ? [
-                'id' => $newsInfoDTO->getEditor()->id,
-                'name' => $newsInfoDTO->getEditor()->name,
+            'editor'             => $newsInfoDTO->getEditor() ? [
+                'id'        => $newsInfoDTO->getEditor()->id,
+                'name'      => $newsInfoDTO->getEditor()->name,
                 'last_name' => $newsInfoDTO->getEditor()->last_name
             ] : null,
-            'language' => $newsInfoDTO->getLanguage(),
-            'relation_id' => $newsInfoDTO->getRelationNewsId(),
-            'image_paths' => $newsInfoDTO->getAttachmentFiles() ?? []
+            'language'           => $newsInfoDTO->getLanguage(),
+            'relation_id'        => $newsInfoDTO->getRelationNewsId(),
+            'image_paths'        => $newsInfoDTO->getAttachmentFiles() ?? []
         ];
     }
 
-    private function getNewsStatus(NewsInfoDTO $newsInfoDTO)
+    private function getStatus(NewsInfoDTO $newsInfoDTO)
     {
-        dd($newsInfoDTO->getStatus()== );
-//        if(trans('news::news.news_statue.' . $newsInfoDTO->getStatus()))
+        if ($newsInfoDTO->getStatus() != 'accept') {
+            return [
+                'fa' => trans('news::news.news_statuses.' . $newsInfoDTO->getStatus()),
+                'en' => $newsInfoDTO->getStatus()
+            ];
+        }
+        if (
+        Carbon::parse($newsInfoDTO->getPublishDate())->lessThanOrEqualTo(Carbon::now())) {
+            return [
+                'fa' => trans('news::news.news_statuses.published'),
+                'en' => 'published'
+            ];
+
+        }
+        return [
+            'fa' => trans('news::news.news_statuses.ready_to_publish'),
+            'en' => 'ready_to_publish'
+        ];
+
+
+    }
+
+    private function isCreatedByUser(NewsInfoDTO $newsInfoDTO)
+    {
+        if (!\Auth::user()) {
+            return false;
+        }
+        if ($newsInfoDTO->getPublisher()->id == \Auth::user()->id) {
+            return true;
+        }
+        return false;
     }
 }
