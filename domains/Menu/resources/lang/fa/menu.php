318c8dc0ff2d65e1e3d3c326a5b277884ec2e83e
Maryam <maryam.b@fanap.plus>
2020-04-21 14:49:15 +0430

    dynamic menu

--- domains/Menu/resources/lang/fa/menu.php
+++ domains/Menu/resources/lang/fa/menu.php
@@ -2,13 +2,14 @@
 
 return [
     'admin_menu' => [
-        "counter"         => 'پیشخوان',
+        "dashboard"       => 'پیشخوان',
         "menu_manager"    => 'مدیریت منوها',
         "event"           => 'رویدادها',
         "news"            => 'اخبار',
         "article"         => 'صفحات ایستا',
         "ehda_card"       => 'کارت‌های اهدای عضو',
         "user"            => 'کاربران',
+        "report"          => 'گزارش‌گیری',
         "profile_setting" => 'تنظیمات حساب',
     ]
 ];
