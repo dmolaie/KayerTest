9eb207fe5637fe2f5be62d2ab3364af372cb53e9
DARIUSH <dariush@fanap.plus>
2020-04-25 14:04:18 +0430

    fix all user report

--- domains/User/src/Http/Controllers/UserController.php
+++ domains/User/src/Http/Controllers/UserController.php
@@ -387,6 +387,23 @@ class UserController extends EhdaBaseController
         }
     }
 
+    public function userReport(UserReportRequest $request,UsersReportPaginateInfoPresenter $usersReportPaginateInfoPresenter)
+    {
+        try {
+            $users =  $this->userService->userReport($request->getReportUserRegister());
+            return $this->response(
+                $usersReportPaginateInfoPresenter->transform($users),
+                Response::HTTP_OK
+            );
+        } catch (ModelNotFoundException $exception) {
+            return $this->response(
+                [],
+                Response::HTTP_NOT_FOUND,
+                trans('user::response.user_not_found')
+            );
+        }
+    }
+
     public function allUserReport(UserReportRequest $request,UserInfoReportPresenter $userInfoReportPresenter)
     {
         try {
