a8e14048739b37e33ae838d1947023a34f26aeb0
Mohammad Soleimanian <mohamad.s@fanap.plus>
2020-05-02 14:14:58 +0430

    front/change category structure - panel

--- resources/BackOffice/components/Category.vue
+++ resources/BackOffice/components/Category.vue
@@ -8,17 +8,18 @@
                  :class="[
                      'fa' === lang ? 'direction-rtl' : 'direction-ltr',
                      !item.is_active ? 'category__item--disable' : '',
+
                  ]"
                  :style="`padding-${'fa' === lang ? 'right' : 'left'}: ${item.gap}px`"
             >
-                <label class="inline-flex items-center cursor-pointer">
+                <label class="inline-flex items-center cursor-pointer"
+                       @click.prevent="onClickCategoriesItems( item )"
+                >
                     <input type="checkbox"
                            class="category__input none"
                            :disabled="!item.is_active"
-                           :checked="item.checked"
                            :value="item.id"
                            v-model="selectedItems"
-                           @change="() => {onChangeCheckboxField(); item.checked = !item.checked}"
                     />
                     <span class="category__checkbox relative border border-solid rounded-1/2 flex-shrink-0"
                     > </span>
@@ -26,7 +27,7 @@
                           v-text="'fa' === lang ? item.name_fa : item.name_en"
                     > </span>
                 </label>
-                <template v-if="!!selectedItems.length">
+                <template v-if="!!selectedItems.length && !hasLength( item.children_ids )">
                     <span v-if="isMainCategory( item.id )"
                           class="category__label font-1xs font-bold text-green-200 cursor-default text-center"
                           :class="[ 'fa' === lang ? 'm-r-auto' : 'm-l-auto' ]"
@@ -48,7 +49,7 @@
 
 <script>
     import {
-        HasLength, CopyOf
+        Length, HasLength, CopyOf
     } from "@vendor/plugin/helper";
 
     export default {
@@ -72,15 +73,19 @@
         },
         watch: {
             value( newVal ) {
-                if ( HasLength( newVal ) )
+                if ( HasLength( newVal ) ) {
                     this.$set(this, 'selectedItems', CopyOf( newVal ))
+                }
             }
         },
         methods: {
+            hasLength( payload ) {
+                return HasLength( payload )
+            },
             reset() {
                 this.$set(this, 'selectedItems', []);
             },
-            onChangeCheckboxField() {
+            emitter() {
                 this.$emit('change', this.selectedItems)
             },
             isMainCategory( category_id ) {
@@ -99,9 +104,45 @@
                     if ( index >= 0 ) {
                         this.selectedItems.splice(index, 1);
                         this.selectedItems.unshift( category_id );
-                        this.onChangeCheckboxField();
+                        this.emitter();
                     }
                 } catch (e) {}
+            },
+            toggleParentItem( parent_id ) {
+                const { selectedItems, list } = this;
+                const LIST = Object.values( list );
+                const PARENT_ITEM = LIST.find(({ id }) => id === parent_id);
+                if ( !!PARENT_ITEM ) {
+                    const IS_CHECKED = PARENT_ITEM['children_ids'].some(id => selectedItems.includes( id ));
+                    if ( !IS_CHECKED ) {
+                        const PARENT_INDEX = selectedItems.findIndex(id => id === parent_id);
+                        this.$delete(selectedItems, PARENT_INDEX);
+                    } else {
+                        !selectedItems.includes(PARENT_ITEM.id) && this.$set(selectedItems, Length( selectedItems ), PARENT_ITEM.id);
+                    }
+                }
+            },
+            onClickCategoriesItems( item ) {
+                const { selectedItems } = this;
+                const IS_CHECKED = selectedItems.includes( item['id'] );
+                if ( !IS_CHECKED ) {
+                    const SET = new Set([
+                        ...selectedItems,
+                        ...item['children_ids'],
+                        item['id']
+                    ]);
+                    this.$set(this, 'selectedItems', [...SET]);
+                }
+                else {
+                    const SET = new Set([
+                        ...selectedItems
+                    ]);
+                    item['children_ids'].forEach(id => SET.delete( id ));
+                    SET.delete(item['id']);
+                    this.$set(this, 'selectedItems', [...SET]);
+                }
+                !!item.parent_id && this.toggleParentItem( item.parent_id );
+                this.emitter();
             }
         },
     }
