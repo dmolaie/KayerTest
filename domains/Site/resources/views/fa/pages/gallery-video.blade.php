d8ab93bc5dedbf1a91adcca62fba301a76b6f6d5
Mohammad Soleimanian <mohamad.s@fanap.plus>
2020-05-09 00:22:39 +0430

    front/dynamic video gallery

--- domains/Site/resources/views/fa/pages/gallery-video.blade.php
+++ domains/Site/resources/views/fa/pages/gallery-video.blade.php
@@ -1,4 +1,11 @@
 @extends('fa.template.master')
+    @php
+        function getVideoUUIDFromPatch( $item ) {
+            if ( !$item ) return '';
+            $url = parse_url($item['link']);;
+            return str_replace('/v/', '', $url['path']);
+        }
+    @endphp
     @section('content')
         <div class="gao-page i-page">
             <div class="container sm:p-0">
@@ -11,17 +18,17 @@
                     <div class="flex-1">
                         <div class="inner-box inner-box--white p-0 m-t-0 text-right overflow-hidden">
                             <div id="video_player">
-                                <img src="https://ehda.center/modules/ehdafront/images/video-placeholder.png"
+                                <img src="{{ secure_asset('/images/video-placeholder.png') }}"
                                      alt="video-placeholder"
                                      class="w-full block"
                                 >
                             </div>
                             <div class="gao-page__content cursor-default">
                                 <h2 class="i-page__sub-title text-blue font-24 font-bold cursor-default m-b-26 sm:font-sm">
-                                    {{$mediaDetail->getFirstTitle()}}
+                                    {{ $mediaDetail->getFirstTitle() }}
                                 </h2>
                                 <p class="gao-page__description text-blue-800 font-base font-medium sm:font-sm">
-                                    {{$mediaDetail->getDescription()}}
+                                    {{ $mediaDetail->getDescription() }}
                                 </p>
                                 <div class="flex items-center md:flex-col">
                                     <div class="i-page__short inline-flex items-center l:m-r-auto md:m-b-20 sm:flex-col">
@@ -56,28 +63,30 @@
                             </div>
                         </div>
                     </div>
-                    <div class="gao-page__aside xxl:w-1/5 xl:w-1/4 w-1/3 md:w-full">
-                        <div class="relative inner-box inner-box--white h-full p-0 m-t-0 overflow-auto">
-                            <div class="episodes absolute w-full">
-                                @foreach($mediaList as $mediaInfo)
-                                    <div class="episode episode--active relative flex items-center cursor-pointer bg-white l:transition-background"
-                                     data-id="PtWzy"
-                                >
-                                    <figure class="episode__cover relative flex-shrink-0 rounded-1/2 has-skeleton pointer-event-none">
-                                        <img src=""
-                                             data-src="https://ehda.center/uploads/posts/gallery/video/image/1527515548_zghqybMDICbyCvwmszW74Rh4hMf4Vh_posttype-XAGkA-150x85.JPG"
-                                             alt="درد بی مغزی"
-                                             class="episode__image block absolute w-full h-full rounded-inherit object-cover"
+                    @if(count($mediaDetail->getContentFiles()) > 1)
+                        <div class="gao-page__aside xxl:w-1/5 xl:w-1/4 w-1/3 md:w-full">
+                            <div class="relative inner-box inner-box--white h-full p-0 m-t-0 overflow-auto">
+                                <div class="episodes absolute w-full">
+                                    @foreach($mediaDetail->getContentFiles() as $index=>$nestedMedia)
+                                        <div class="episode relative flex items-center cursor-pointer bg-white l:transition-background {{ !$index ? 'episode--active' : '' }}"
+                                         data-id="{{ getVideoUUIDFromPatch( $nestedMedia ) }}"
                                         >
-                                    </figure>
-                                    <span class="episode__caption text-blue-800 text-blue-800 font-sm font-medium pointer-event-none">
-                                        درد بی مغزی
-                                    </span>
+                                            <figure class="episode__cover relative flex-shrink-0 rounded-1/2 has-skeleton pointer-event-none">
+                                                <img src=""
+                                                     data-src="{{ $nestedMedia['path'] }}"
+                                                     alt="{{$mediaDetail->getFirstTitle()}}"
+                                                     class="episode__image block absolute w-full h-full rounded-inherit object-cover"
+                                                >
+                                            </figure>
+                                            <span class="episode__caption text-blue-800 text-blue-800 font-sm font-medium pointer-event-none">
+                                                {{ $nestedMedia['title'] ? $nestedMedia['title'] : $mediaDetail->getFirstTitle() }}
+                                            </span>
+                                        </div>
+                                    @endforeach
                                 </div>
-                                @endforeach
                             </div>
                         </div>
-                    </div>
+                    @endif
                 </div>
             </div>
         </div>
@@ -85,7 +94,7 @@
 
     @section('scripts')
         <script type="text/JavaScript" id="aparat_script"
-                src="https://www.aparat.com/embed/PtWzy?data[rnddiv]=video_player&data[responsive]=yes">
+                src="https://www.aparat.com/embed/{{ getVideoUUIDFromPatch($mediaDetail->getContentFiles()[0]) }}?data[rnddiv]=video_player&data[responsive]=yes">
         </script>
-        <script src="{{secure_asset('js/site/gallery-video.js')}}" defer></script>
+        <script src="{{ secure_asset('js/site/gallery-video.js') }}" defer></script>
     @endsection
