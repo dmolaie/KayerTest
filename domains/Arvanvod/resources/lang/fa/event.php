b4452decfc27661991d1846856da4112d238b561
DARIUSH <dariush@fanap.plus>
2020-05-15 11:30:12 +0430

    Payment-Arvanvod

---
+++ domains/Arvanvod/resources/lang/fa/event.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'event_statuses' => [
+        'accept'           => 'تایید شده',
+        'ready_to_publish' => 'در انتظار انتشار',
+        'reject'           => 'رد شده',
+        'pending'          => 'در انتظار تایید',
+        'published'        => 'منتشر شده',
+        'cancel'           => 'لغو شده',
+        'recycle'          => 'در زباله‌دان',
+        'delete'           => 'حذف شده'
+    ],
+];
---
+++ domains/Payment/database/migrations/2020_05_06_124720_payment.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Payment extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->integerIncrements('id');
+            $table->string('uuid')->unique()->index();
+            $table->bigInteger('amount')->nullable();
+            $table->boolean('status')->nullable();
+            $table->dateTime('invoice_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+}
