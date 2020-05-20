837c0f4957c05d1ec0c4b7a97f45dcd5a8edce17
Maryam <maryam.b@fanap.plus>
2020-05-20 12:37:44 +0430

    sms register

--- domains/SmsRegister/src/Repositories/SmsRegisterRepository.php
+++ domains/SmsRegister/src/Repositories/SmsRegisterRepository.php
@@ -15,12 +15,13 @@ class SmsRegisterRepository
     {
         $smsRegister = $this->entityName::where(
             'national_code', $smsRegisterDTO->getNationalCode()
-        )->first();
+        )->where('status', 'not_complete')->first();
         $smsRegister = $smsRegister ?? new $this->entityName;
         $smsRegister->mobile_number = $smsRegisterDTO->getMobileNumber();
         $smsRegister->channelType = $smsRegisterDTO->getChannelType();
         $smsRegister->national_code = $smsRegisterDTO->getNationalCode();
         $smsRegister->request_content = $smsRegisterDTO->getFirstRequestContent();
+        $smsRegister->status = 'not_complete';
         if ($smsRegister->getDirty()) {
             $smsRegister->save();
         }
@@ -30,15 +31,17 @@ class SmsRegisterRepository
     public function getUserNationalCode(SmsRegisterDTO $smsRegisterDTO)
     {
         $smsRegister = $this->entityName::where(
-            'mobile_number', $smsRegisterDTO->getMobileNumber())
+            'mobile_number', $smsRegisterDTO->getMobileNumber())->where('status', 'not_complete')
             ->firstOrFail();
-        if($smsRegisterDTO->getSecondRequestContent()){
+        if ($smsRegisterDTO->getSecondRequestContent()) {
             $smsRegister->second_request_content = $smsRegisterDTO->getSecondRequestContent();
-            $smsRegister->birth_date=$smsRegisterDTO->getBirthDate();
+            $smsRegister->birth_date = $smsRegisterDTO->getBirthDate();
+            $smsRegister->status = 'not_complete';
 
         }
         if ($smsRegisterDTO->getThirdRequestContent()) {
             $smsRegister->third_request_content = $smsRegisterDTO->getThirdRequestContent();
+            $smsRegister->status = 'not_complete';
 
         }
         if ($smsRegister->getDirty()) {
@@ -46,4 +49,19 @@ class SmsRegisterRepository
         }
         return $smsRegister;
     }
+
+    public function changeToComplete($nationalCode)
+    {
+        $smsRegisters = $this->entityName::where(
+            'national_code', $nationalCode
+        )->where('status' . 'not_complete')->get();
+        foreach ($smsRegisters as $smsRegister) {
+            $smsRegister->status = 'complete';
+            if ($smsRegister->getDirty()) {
+                $smsRegister->save();
+            }
+        }
+        return;
+    }
+
 }
