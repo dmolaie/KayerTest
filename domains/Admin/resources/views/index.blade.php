a0038e61b4a3666406910e351f5e04c1ee0219db
Maryam <maryam.b@fanap.plus>
2020-06-02 18:21:11 +0430

    asset fix

--- domains/Admin/resources/views/index.blade.php
+++ domains/Admin/resources/views/index.blade.php
@@ -9,32 +9,32 @@
     <meta name="msapplication-navbutton-color" content="#004883">
     <meta name="apple-mobile-web-app-status-bar-style" content="#004883">
     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
-    <meta name="msapplication-TileImage" content="{{ asset('images/favicon/ms-icon-144x144.png?v=c298c7f8233d') }}">
-    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('images/favicon/apple-icon-180x180.png?v=c298c7f8233d') }}">
-    <link rel="apple-touch-icon" sizes="152x152" href="{{ asset('images/favicon/apple-icon-152x152.png?v=c298c7f8233d') }}">
-    <link rel="apple-touch-icon" sizes="144x144" href="{{ asset('images/favicon/apple-icon-144x144.png?v=c298c7f8233d') }}">
-    <link rel="apple-touch-icon" sizes="120x120" href="{{ asset('images/favicon/apple-icon-120x120.png?v=c298c7f8233d') }}">
-    <link rel="apple-touch-icon" sizes="114x114" href="{{ asset('images/favicon/apple-icon-114x114.png?v=c298c7f8233d') }}">
-    <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('images/favicon/apple-icon-76x76.png?v=c298c7f8233d') }}">
-    <link rel="apple-touch-icon" sizes="72x72" href="{{ asset('images/favicon/apple-icon-72x72.png?v=c298c7f8233d') }}">
-    <link rel="apple-touch-icon" sizes="60x60" href="{{ asset('images/favicon/apple-icon-60x60.png?v=c298c7f8233d') }}">
-    <link rel="apple-touch-icon" sizes="57x57" href="{{ asset('images/favicon/apple-icon-57x57.png?v=c298c7f8233d') }}">
-    <link rel="icon" type="image/png" sizes="192x192"  href="{{ asset('images/favicon/android-icon-192x192.png?v=c298c7f8233d') }}">
-    <link rel="icon" type="image/png" sizes="96x96" href="{{ asset('images/favicon/favicon-96x96.png?v=c298c7f8233d') }}">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/favicon/favicon-32x32.png?v=c298c7f8233d') }}">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/favicon/favicon-16x16.png?v=c298c7f8233d') }}">
-    <link rel="icon" type="image/x-icon" href="{{ asset('images/favicon/favicon.ico?v=c298c7f8233d') }}">
-    <link href="{{ asset('images/favicon/favicon.ico?v=c298c7f8233d') }}" rel="shortcut icon">
+    <meta name="msapplication-TileImage" content="{{ secure_asset('images/favicon/ms-icon-144x144.png?v=c298c7f8233d') }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ secure_asset('images/favicon/apple-icon-180x180.png?v=c298c7f8233d') }}">
+    <link rel="apple-touch-icon" sizes="152x152" href="{{ secure_asset('images/favicon/apple-icon-152x152.png?v=c298c7f8233d') }}">
+    <link rel="apple-touch-icon" sizes="144x144" href="{{ secure_asset('images/favicon/apple-icon-144x144.png?v=c298c7f8233d') }}">
+    <link rel="apple-touch-icon" sizes="120x120" href="{{ secure_asset('images/favicon/apple-icon-120x120.png?v=c298c7f8233d') }}">
+    <link rel="apple-touch-icon" sizes="114x114" href="{{ secure_asset('images/favicon/apple-icon-114x114.png?v=c298c7f8233d') }}">
+    <link rel="apple-touch-icon" sizes="76x76" href="{{ secure_asset('images/favicon/apple-icon-76x76.png?v=c298c7f8233d') }}">
+    <link rel="apple-touch-icon" sizes="72x72" href="{{ secure_asset('images/favicon/apple-icon-72x72.png?v=c298c7f8233d') }}">
+    <link rel="apple-touch-icon" sizes="60x60" href="{{ secure_asset('images/favicon/apple-icon-60x60.png?v=c298c7f8233d') }}">
+    <link rel="apple-touch-icon" sizes="57x57" href="{{ secure_asset('images/favicon/apple-icon-57x57.png?v=c298c7f8233d') }}">
+    <link rel="icon" type="image/png" sizes="192x192"  href="{{ secure_asset('images/favicon/android-icon-192x192.png?v=c298c7f8233d') }}">
+    <link rel="icon" type="image/png" sizes="96x96" href="{{ secure_asset('images/favicon/favicon-96x96.png?v=c298c7f8233d') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ secure_asset('images/favicon/favicon-32x32.png?v=c298c7f8233d') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ secure_asset('images/favicon/favicon-16x16.png?v=c298c7f8233d') }}">
+    <link rel="icon" type="image/x-icon" href="{{ secure_asset('images/favicon/favicon.ico?v=c298c7f8233d') }}">
+    <link href="{{ secure_asset('images/favicon/favicon.ico?v=c298c7f8233d') }}" rel="shortcut icon">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link href="{{ asset('css/appStyle.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('css/appStyle.css') }}" rel="stylesheet">
     <title>
         انجمن اهدای عضو ایرانیان
     </title>
 </head>
 <body class="h-full overflow-x-hidden">
     <div id="app"></div>
-    <script src="{{ asset('js/office/runtime.js ') }}" defer></script>
-    <script src="{{ asset('js/office/vendors~app.js') }}" defer></script>
-    <script src="{{ asset('js/office/app.js') }}" defer></script>
+    <script src="{{ secure_asset('js/office/runtime.js ') }}" defer></script>
+    <script src="{{ secure_asset('js/office/vendors~app.js') }}" defer></script>
+    <script src="{{ secure_asset('js/office/app.js') }}" defer></script>
 </body>
 </html>
